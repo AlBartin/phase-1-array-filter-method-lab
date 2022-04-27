876ef604cc1e750c58220e985692f5666771e8bb
alan <alan.martnz@gmail.com>
2022-04-27 16:01:08 -0700

    commit tree 173dacc9651ed42646e4f8be24bc7fe2d22fcffd

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching ( driversArray, driversName ) {
+    return driversArray.filter(x => x.toUpperCase() === driversName.toUpperCase());
+};
+
+function fuzzyMatch ( driversArray, driversName ) {
+    return driversArray.filter(x => x.slice(0,1) === driversName.slice(0,1))
+};
+
+function matchName ( driversArray, driversName ) {
+    return driversArray.filter( x => x.name === driversName)
+};
